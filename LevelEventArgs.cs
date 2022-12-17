8f3f2cdf7d64259d54bd95e825e1e79d051c9f78
Nikita <mrniki2396@gmail.com>
2022-12-17 15:24:26 +0700

    премер

---
+++ LevelEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ANTWARS
+{
+	public class LevelEventArgs : EventArgs
+	{
+		private Levels oldLevel;
+		private Levels newLevel;
+		public Levels OldLevel { get { return oldLevel; } }
+		public Levels NewLevel { get { return newLevel; } }
+		
+		public LevelEventArgs(Levels oldLevel, Levels newLevel)
+		{
+			this.oldLevel = oldLevel;
+			this.newLevel = newLevel;
+		}
+	}
+}
