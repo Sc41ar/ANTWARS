40f1986132f390e9e8d1192d0ec52ef99be0cc44
Sc41ar <mrniki2396@gmail.com>
2022-12-18 20:22:47 +0700

    Добавил абстрактный класс

--- Enemy.cs
+++ Enemy.cs
@@ -3,11 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace ANTWARS
 {
-	internal class Enemy : NeutralColony
+	internal abstract class Enemy : NeutralColony
 	{
+		public Enemy()
+		{
+			Size = new Size(75, 75);
+			Population = 20;
+			PopulationGrowthSpeed = 1;
+			PopulationLimit = 20;
+		}
 
+		public Enemy(int population, int populationLimit, Levels level)
+		{
+			Level = level;
+			Population = population;
+			PopulationLimit = populationLimit;
+			Size = new Size(75, 75);
+			Text = Population + "/" + PopulationLimit;
+
+		}
+
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			base.OnPaint(e);	
+
+		}
 	}
 }
